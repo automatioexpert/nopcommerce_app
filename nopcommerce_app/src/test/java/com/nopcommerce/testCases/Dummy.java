e188e47359b292ec0b6885d19871646832aabd0a
Valmiki Vishwakarma <you@example.com>
2022-12-19 21:11:20 +0530

    add.

---
+++ nopcommerce_app/src/test/java/com/nopcommerce/testCases/Dummy.java
@@ -0,0 +1,20 @@
+package com.nopcommerce.testCases;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class Dummy {
+	
+	@Test
+	void test123()
+	{
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	void test1234()
+	{
+		Assert.assertTrue(true);
+	}
+
+}
